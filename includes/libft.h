6fe82403525c7e27154d6909f69494db9d881b90
eliott dhommee <eliottdhommee@MacBook-Air-de-eliott.local>
2017-07-13 11:21:56 +0200

    ajout gnl et pf dans libft.h +debut btree

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 09:40:27 by edhommee          #+#    #+#             */
-/*   Updated: 2016/11/21 12:53:29 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/07/13 11:20:41 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,5 +91,18 @@ void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(f)(t_list *elem));
+
+int					get_next_line(const int fd, char **line);
+int					ft_printf(const char *format, ...);
+
+typedef	struct		s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}					t_btree;
+
+t_btree				*btree_create_node(void *item);
+void				btree_apply_prefix(t_btree *root, void(*applyf)(void*));
 
 #endif
---
+++ src/btree_create_node.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree_create_node.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edhommee <eliottdhommee@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/11 16:03:00 by edhommee          #+#    #+#             */
+/*   Updated: 2017/07/13 10:01:08 by edhommee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+t_btree		*btree_create_node(void *item)
+{
+	t_btree		*new_node;
+
+	new_node = malloc(sizeof(t_btree));
+	if (!new_node)
+		return (NULL);
+	else
+	{
+		new_node->item = item;
+		new_node->left = NULL;
+		new_node->right = NULL;
+	}
+	return(new_node);
+}
