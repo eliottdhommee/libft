63b40f7cb28c913551b3c10cb7d2da2a21e3eb3d
eliott dhommee <eliottdhommee@MacBook-Air-de-eliott.local>
2017-07-15 15:22:40 +0200

    chmod

--- src/btree_apply_by_level.c
+++ src/btree_apply_by_level.c
@@ -6,14 +6,26 @@
 /*   By: edhommee <eliottdhommee@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 13:14:46 by edhommee          #+#    #+#             */
-/*   Updated: 2017/07/15 13:24:56 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/07/15 14:10:26 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void		btree_apply_by_level(t_btree *root, void (*applyf)
+static void		btree_apply_one_level()
+{
+
+}
+
+void			btree_apply_by_level(t_btree *root, void (*applyf)
 					(void *item, int current_level, int is_first_elem))
 {
-	
+	int		i;
+
+	i = 0;
+	while (i <= btree_level_count(root))
+	{
+		btree_apply_one_level();
+		i++;
+	}
 }
