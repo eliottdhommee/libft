4031352d946000bc00f56d440f705110a3c96415
eliott dhommee <eliottdhommee@MacBook-Air-de-eliott.local>
2017-09-07 22:50:20 +0200

    oui

--- srcs/btree/btree_insert_data.c
+++ srcs/btree/btree_insert_data.c
@@ -6,7 +6,7 @@
 /*   By: edhommee <eliottdhommee@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/13 11:42:33 by edhommee          #+#    #+#             */
-/*   Updated: 2017/07/20 14:21:45 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/09/07 22:08:29 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	btree_insert_data(t_btree **root, void *item, int (*cmpf)(void*, void*))
 		*root = btree_create_node(item);
 	else
 	{
-		if (cmpf(((t_btree *)*root)->item, item) > 0)
+		if (cmpf(((t_btree *)*root)->item, item) >= 0)
 			btree_insert_data(&((t_btree *)*root)->left, item, (*cmpf));
 		else
 			btree_insert_data(&((t_btree *)*root)->right, item, (*cmpf));
