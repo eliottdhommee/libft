fdce01b48e65a87c914f65554ed9d3114fc9d2e9
Eliott D'HOMMEE <edhommee@e2r10p9.42.fr>
2017-09-08 16:42:27 +0200

    non

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 09:31:49 by edhommee          #+#    #+#             */
-/*   Updated: 2017/05/20 17:25:09 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/09/08 15:14:24 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int		i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	while (*s1 && *s2  && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
