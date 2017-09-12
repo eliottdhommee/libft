fd123d33fbd3d24325647a17b4c07bf71e40f4f0
Eliott D'HOMMEE <edhommee@e2r10p9.42.fr>
2017-09-12 10:18:58 +0200

    norme

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 09:22:00 by edhommee          #+#    #+#             */
-/*   Updated: 2017/09/09 04:58:33 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/09/12 09:55:19 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memset(void *s, int c, size_t n)
 		return (s);
 	dest = (unsigned char *)s;
 	dest[n - 1] = (unsigned char)c;
-	while (*dest  != (unsigned char)c)
+	while (*dest != (unsigned char)c)
 		*dest++ = (unsigned char)c;
 	return (s);
 }
