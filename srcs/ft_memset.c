761a82fced03ac007797b140a4e2d42eb63cb822
Eliott D'HOMMEE <edhommee@e2r10p9.42.fr>
2017-09-12 11:46:35 +0200

    memset

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 09:22:00 by edhommee          #+#    #+#             */
-/*   Updated: 2017/09/12 09:55:19 by edhommee         ###   ########.fr       */
+/*   Updated: 2017/09/12 11:42:48 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	*ft_memset(void *s, int c, size_t n)
 	if (!n)
 		return (s);
 	dest = (unsigned char *)s;
-	dest[n - 1] = (unsigned char)c;
-	while (*dest != (unsigned char)c)
+	while (n--)
 		*dest++ = (unsigned char)c;
 	return (s);
 }
